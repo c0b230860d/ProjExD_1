12f7f9f6ee801e06e9a4135429a3d81a1801e270
c0b23086 <c0b230860d@edu.teu.ac.jp>
2024-06-11 16:32:34 +0900

    練習7:ワープ対策

--- pygame_image.py
+++ pygame_image.py
@@ -11,7 +11,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #loadで背景画像を読み込む
-    kk_img = pg.image.load("fig/3.png")
+    bg_img_han = pg.transform.flip(bg_img,True,False)
+    kk_img = pg.image.load("fig/3.png") #こうとん画像を読み込む
     kk_img = pg.transform.flip(kk_img, True, False)
 
     tmr = 0
@@ -21,8 +22,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 800
+        x = tmr % (3200)
         screen.blit(bg_img, [-x, 0]) #(0,0)左上に背景を張り付け
+        screen.blit(bg_img_han, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0]) 
+        screen.blit(bg_img_han, [-x+(1600*3), 0])
         # screen.blit(kk_img,[300, 200])これだと固定されてしまうので・・・
         kk_rect = kk_img.get_rect() # こうかとんrectを取得
         kk_rect.center = 300, 200 # 座標設定
